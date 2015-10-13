1ba7d364c3a72e57e32d2db58c53290ffef002ab
Jonathan Sutton <comp8nerd4u2@gmail.com>
2015-10-13 09:09:04 -0500

    Added a few mock classes to develop shape of game

---
+++ monster-rpg/Game/MonsterRPG.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterRPG {
+    class MonsterRPG {
+        public static void main(String[] argsv) {
+
+        }
+    }
+}
---
+++ monster-rpg/Game/TitleScreen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonsterRPG {
+    /// <summary>
+    /// Represents title screen for our game.
+    /// </summary>
+    class TitleScreen {
+    }
+}
