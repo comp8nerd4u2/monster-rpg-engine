a3aa77d38f4b9f67cd309dd5a0788afde2f5da4a
Jonathan Sutton <comp8nerd4u2@gmail.com>
2015-10-20 09:14:24 -0500

    Renamed threads, disposed COM objects before termination, and added more documentation

--- monster-rpg-engine/MonsterRPGEngine/Engine.cs
+++ monster-rpg-engine/MonsterRPGEngine/Engine.cs
@@ -20,8 +20,10 @@ namespace MonsterRPGEngine {
     class Engine {
         public static RenderForm gameWindow;
 
+        private static D3D11Device device;
         private static SwapChain swapChain;
         private static RenderTarget renderTarget;
+        private static Boolean ShouldDisplayFPS = true; //Placeholder until FPS algorithm is added
 
         long gameTime = 0L;
         Boolean shouldTerminate = false;
@@ -29,43 +31,52 @@ namespace MonsterRPGEngine {
         /// Load game assets and prepare to start
         /// </summary>
         public void Init() {
-            //Run the game window on its own thread
-            Thread messageLoopThread = new Thread(() => {
-                //Application.Run(gameWindow);
+            /* 
+             * Create a new thread for our game window.
+             * Initializing in a new thread complicates our code,
+             * but it is worth having seperate UI and Engine threads
+             */
+            Thread.CurrentThread.Name = "EngineThread";
+            Thread UIThread = new Thread(() => {
                 gameWindow = new RenderForm("MonsterRPGEngine");
-                gameWindow.FormClosed += (x, y) => {
+                gameWindow.FormClosed += (o, e) => {
                     Terminate();
                 };
                 //TODO: Learn how to initialize DirectX components
+                //Initialize here because WinForms wont let us do it outside UI thread
+                //Describing our swap chain allows us to tell DirectX exactly how we want to render and transition each frame
                 SwapChainDescription swapChainDesc = new SwapChainDescription() {
                     BufferCount = 2, //Double buffer
                     Usage = Usage.RenderTargetOutput, //We're using this swap chain as a RenderTarget
                     OutputHandle = gameWindow.Handle, //Pass handle of our game window
                     IsWindowed = true, //Yep
-                    ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.B8G8R8A8_UNorm),
+                    ModeDescription = new ModeDescription(0, 0, new Rational(60, 1), Format.B8G8R8A8_UNorm), //Automatic resolution resizing, Refresh rate, Pixel color format
                     SampleDescription = new SampleDescription(1, 0), //No Multisampling
-                    Flags = SwapChainFlags.AllowModeSwitch,
+                    Flags = SwapChainFlags.AllowModeSwitch, //Allow us to switch modes
                     SwapEffect = SwapEffect.Discard //Assuming this means buffer will be cleared when it is moved offscreen
                 };
-                D3D11Device device;
-                //SwapChain swapChain;
+                //We need to create a Direct3D11 device because we can't create a swap chain for Direct2D1 without it. Direct2D1 has no Device object.
                 D3D11Device.CreateWithSwapChain(SharpDX.Direct3D.DriverType.Hardware, SharpDX.Direct3D11.DeviceCreationFlags.BgraSupport, swapChainDesc, out device, out swapChain);
-                Surface backbuffer = Surface.FromSwapChain(swapChain, 0);
+                Surface backbuffer = Surface.FromSwapChain(swapChain, 0); //Grab the backbuffer to use as a RenderTarget
+                //Need a Direct2D1 factory just to make a new RenderTarget
                 D2D1Factory factory = new D2D1Factory(FactoryType.SingleThreaded, DebugLevel.Error);
                 Size2F dpi = factory.DesktopDpi;
                 renderTarget = new RenderTarget(factory, backbuffer, new RenderTargetProperties() {
-                    DpiX = dpi.Width,
-                    DpiY = dpi.Height,
-                    MinLevel = FeatureLevel.Level_DEFAULT,
-                    PixelFormat = new PixelFormat(Format.Unknown, AlphaMode.Ignore),
-                    Type = RenderTargetType.Default,
-                    Usage = RenderTargetUsage.None
+                    DpiX = dpi.Width, //Horizontal DPI
+                    DpiY = dpi.Height, //Vertical DPI
+                    MinLevel = FeatureLevel.Level_DEFAULT, //Just make sure we are using a hardware accelerated render target
+                    PixelFormat = new PixelFormat(Format.Unknown, AlphaMode.Ignore), //Who cares? our swap chain already knows this stuff
+                    Type = RenderTargetType.Default, //Again swap chain has this
+                    Usage = RenderTargetUsage.None //Yeah that swap chain though
                 });
+                //Wow that was a handful. Now we can start our message loop on this thread.
                 gameWindow.Show();
                 Application.Run(gameWindow);
             });
-            messageLoopThread.SetApartmentState(ApartmentState.STA);
-            messageLoopThread.Start();
+            UIThread.SetApartmentState(ApartmentState.STA);
+            UIThread.Name = "UIThread";
+            UIThread.Start();
+            //TODO: Replace with cleaner multithreading code. This is ugly
             while (gameWindow == null || renderTarget == null) { }
             
             
@@ -91,11 +102,13 @@ namespace MonsterRPGEngine {
         /// </summary>
         public void Render() {
             //TODO: Add render logic here
+            //WARNING: May have to move rendering code to message loop in UIThread if this is causing issues
             Console.Clear();
             Console.WriteLine("Game Time: " + gameTime);
             //Begin Draw
             renderTarget.BeginDraw();
-            renderTarget.Clear(Color4.Black);
+            renderTarget.Clear(Color.CornflowerBlue.ToColor4()); //Clear using a common game clear color
+            
             //End Draw
             renderTarget.EndDraw();
             //Swap Backbuffer
@@ -129,6 +142,10 @@ namespace MonsterRPGEngine {
         /// </summary>
         public void Terminate() {
             //TODO: Destroy and clean up game objects
+            //Release all COM objects
+            device.Dispose();
+            swapChain.Dispose();
+            renderTarget.Dispose();
             Environment.Exit(0);
         }
     }
